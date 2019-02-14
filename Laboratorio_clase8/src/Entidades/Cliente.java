5ff0efc0236eff56aaaa916f8b0e96e6d55758a8
Fabio Alejandro Camargo Díaz <fcamargodiaz@gmail.com>
2019-02-14 16:19:02 -0500

    Entidades getters and settrers de cliente

--- Laboratorio_clase8/src/Entidades/Cliente.java
+++ Laboratorio_clase8/src/Entidades/Cliente.java
@@ -10,5 +10,24 @@ package Entidades;
  * @author alejo
  */
 public class Cliente {
+    private int documento;
+    private String nombre;
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
