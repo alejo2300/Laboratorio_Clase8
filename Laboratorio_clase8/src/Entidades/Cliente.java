67bccdd35ff803e904721e113823c975dc739f9a
Fabio Alejandro Camargo Díaz <fcamargodiaz@gmail.com>
2019-02-14 16:23:34 -0500

    Cuenta||Atributos||Entidades

--- Laboratorio_clase8/src/Entidades/Cliente.java
+++ Laboratorio_clase8/src/Entidades/Cliente.java
@@ -29,5 +29,5 @@ public class Cliente {
         this.nombre = nombre;
     }
     
-    
 }
+
--- Laboratorio_clase8/src/Entidades/Cuenta.java
+++ Laboratorio_clase8/src/Entidades/Cuenta.java
@@ -10,5 +10,24 @@ package Entidades;
  * @author alejo
  */
 public class Cuenta {
+    private String numero;
+    private int saldo;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+    
     
 }
